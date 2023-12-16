163756384a7439d15de9fa159a8db4d493cc2b2f
Gabriel Henrique <gabhenrique0@gmail.com>
2023-12-16 13:54:42 -0300

    Config: create the base structure of the project

---
+++ Shopping Search App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping Search App
+//
+//  Created by Gabriel Pereira on 16/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
