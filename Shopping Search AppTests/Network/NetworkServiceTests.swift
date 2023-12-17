a7238610d7e19f5215d3f4384ebd9a50d988863b
Gabriel Henrique <gabhenrique0@gmail.com>
2023-12-16 22:58:27 -0300

    Test: add scenarios that needs to be tested on request method for network service

--- Shopping Search AppTests/Network/NetworkServiceTests.swift
+++ Shopping Search AppTests/Network/NetworkServiceTests.swift
@@ -19,5 +19,45 @@ final class NetworkServiceTests: QuickSpec {
             urlSessionMock = URLSessionMock()
             sut = NetworkService(urlSession: urlSessionMock)
         }
+        
+        describe("#request") {
+            context("when url request contains components that can't be safely unwrapped") {
+                // TODO: - Add tests here
+            }
+            
+            context("when url request contains components that can be safely unwrapped") {
+                // TODO: - Add tests here
+            }
+            
+            context("when url request contains query components") {
+                // TODO: - Add tests here
+            }
+            
+            context("when url request contains headers") {
+                // TODO: - Add tests here
+            }
+            
+            context("when url request is valid and the data task is called") {
+                context("and error object has value") {
+                    // TODO: - Add tests here
+                }
+                
+                context("and status code is between 200 and 299") {
+                    // TODO: - Add tests here
+                }
+                
+                context("and status code is outside the success range") {
+                    // TODO: - Add tests here
+                }
+                
+                context("and the response response is decoded") {
+                    // TODO: - Add tests here
+                }
+                
+                context("and the response is decoded with an error") {
+                    // TODO: - Add tests here
+                }
+            }
+        }
     }
 }
