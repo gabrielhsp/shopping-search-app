7ba3abfb271ece5fb3de45f9134b4e1b1097d665
Gabriel Henrique <gabhenrique0@gmail.com>
2023-12-16 21:40:54 -0300

    Test: add tests for error response enum

--- Shopping Search AppTests/Network/Enumerators/ErrorResponseTests.swift
+++ Shopping Search AppTests/Network/Enumerators/ErrorResponseTests.swift
@@ -6,18 +6,57 @@
 //
 
 import Foundation
-import XCTest
+import Nimble
+import Quick
 @testable import Shopping_Search_App
 
-final class ErrorResponseTests: XCTest {
-    var sut: ErrorResponse!
-    
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        sut = nil
-        super.tearDown()
+final class ErrorResponseTests: QuickSpec {
+    override class func spec() {
+        var sut: ErrorResponse!
+        
+        describe("#init") {
+            context("when error response is initialized with a 404 value") {
+                beforeEach {
+                    sut = ErrorResponse(rawValue: 404)
+                }
+                
+                it("should initialize sut with an invalid endpoint case") {
+                    expect(sut).to(equal(.invalidEndpoint))
+                    expect(sut.domain).to(equal("Invalid Endpoint"))
+                }
+            }
+            
+            context("when error response is initialized with a 417 value") {
+                beforeEach {
+                    sut = ErrorResponse(rawValue: 417)
+                }
+                
+                it("should initialize sut with an invalid endpoint case") {
+                    expect(sut).to(equal(.invalidResponse))
+                    expect(sut.domain).to(equal("Invalid Response"))
+                }
+            }
+            
+            context("when error response is initialized with a 422 value") {
+                beforeEach {
+                    sut = ErrorResponse(rawValue: 422)
+                }
+                
+                it("should initialize sut with an invalid endpoint case") {
+                    expect(sut).to(equal(.unprocessableEntity))
+                    expect(sut.domain).to(equal("Unprocessable Entity"))
+                }
+            }
+            
+            context("when error response is initialized with an error code that's not mapped") {
+                beforeEach {
+                    sut = ErrorResponse(rawValue: 999)
+                }
+                
+                it("should return a nil object") {
+                    expect(sut).to(beNil())
+                }
+            }
+        }
     }
 }
