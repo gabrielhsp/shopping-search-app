7bd46acade41d49a35907dac1d26fc669a531dc8
Gabriel Henrique <gabhenrique0@gmail.com>
2023-12-16 22:57:53 -0300

    Test: add a describe for init method on http method tests

--- Shopping Search AppTests/Network/Enumerators/HTTPMethodTests.swift
+++ Shopping Search AppTests/Network/Enumerators/HTTPMethodTests.swift
@@ -14,63 +14,65 @@ final class HTTPMethodTests: QuickSpec {
     override class func spec() {
         var sut: HTTPMethod!
         
-        context("when http method is initialized with a GET value") {
-            beforeEach {
-                sut = HTTPMethod(rawValue: "GET")
+        describe("#init") {
+            context("when http method is initialized with a GET value") {
+                beforeEach {
+                    sut = HTTPMethod(rawValue: "GET")
+                }
+                
+                it("should return a get case for http method") {
+                    expect(sut).to(equal(.get))
+                }
             }
             
-            it("should return a get case for http method") {
-                expect(sut).to(equal(.get))
-            }
-        }
-        
-        context("when http method is initialized with a POST value") {
-            beforeEach {
-                sut = HTTPMethod(rawValue: "POST")
-            }
-            
-            it("should return a post case for http method") {
-                expect(sut).to(equal(.post))
-            }
-        }
-        
-        context("when http method is initialized with a PUT value") {
-            beforeEach {
-                sut = HTTPMethod(rawValue: "PUT")
+            context("when http method is initialized with a POST value") {
+                beforeEach {
+                    sut = HTTPMethod(rawValue: "POST")
+                }
+                
+                it("should return a post case for http method") {
+                    expect(sut).to(equal(.post))
+                }
             }
             
-            it("should return a put case for http method") {
-                expect(sut).to(equal(.put))
-            }
-        }
-        
-        context("when http method is initialized with a PATCH value") {
-            beforeEach {
-                sut = HTTPMethod(rawValue: "PATCH")
-            }
-            
-            it("should return a put case for http method") {
-                expect(sut).to(equal(.patch))
-            }
-        }
-        
-        context("when http method is initialized with a DELETE value") {
-            beforeEach {
-                sut = HTTPMethod(rawValue: "DELETE")
+            context("when http method is initialized with a PUT value") {
+                beforeEach {
+                    sut = HTTPMethod(rawValue: "PUT")
+                }
+                
+                it("should return a put case for http method") {
+                    expect(sut).to(equal(.put))
+                }
             }
             
-            it("should return a delete case for http method") {
-                expect(sut).to(equal(.delete))
-            }
-        }
-        
-        context("when http method is initialized with an unmatched value") {
-            beforeEach {
-                sut = HTTPMethod(rawValue: "INVALID_METHOD")
+            context("when http method is initialized with a PATCH value") {
+                beforeEach {
+                    sut = HTTPMethod(rawValue: "PATCH")
+                }
+                
+                it("should return a put case for http method") {
+                    expect(sut).to(equal(.patch))
+                }
             }
             
-            it("should return a nil object") {
-                expect(sut).to(beNil())
+            context("when http method is initialized with a DELETE value") {
+                beforeEach {
+                    sut = HTTPMethod(rawValue: "DELETE")
+                }
+                
+                it("should return a delete case for http method") {
+                    expect(sut).to(equal(.delete))
+                }
+            }
+            
+            context("when http method is initialized with an unmatched value") {
+                beforeEach {
+                    sut = HTTPMethod(rawValue: "INVALID_METHOD")
+                }
+                
+                it("should return a nil object") {
+                    expect(sut).to(beNil())
+                }
             }
         }
     }
