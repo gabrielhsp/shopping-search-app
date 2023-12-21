705a1a99d2ba21c15c9164a9c08dd9200f547a25
Gabriel Henrique <gabhenrique0@gmail.com>
2023-12-20 21:26:04 -0300

    Test: create the basic structure for shopping search repository tests

--- Shopping Search AppTests/Scenes/Search/Repository/ShoppingSearchRepositoryTests.swift
+++ Shopping Search AppTests/Scenes/Search/Repository/ShoppingSearchRepositoryTests.swift
@@ -12,10 +12,48 @@ import Quick
 
 final class ShoppingSearchRepositoryTests: QuickSpec {
     override class func spec() {
+        var networkServiceMock: NetworkServiceMock!
         var sut: ShoppingSearchRepository!
         
         beforeEach {
-            sut = ShoppingSearchRepository(networkService: <#T##NetworkServiceType#>)
+            networkServiceMock = NetworkServiceMock()
+            sut = ShoppingSearchRepository(networkService: networkServiceMock)
+        }
+        
+        describe("#requestSearch") {
+            let dummySearchTerm = "dummy"
+            var completionCallArgs: [Result<ShoppingSearchModel, Error>] = []
+            
+            afterEach {
+                completionCallArgs = []
+            }
+            
+            context("when network service response returns a failure result") {
+                var dummyError = NSError(domain: ErrorResponse.unprocessableEntity.domain, code: ErrorResponse.unprocessableEntity.rawValue)
+                
+                beforeEach {
+                    networkServiceMock.requestResult = .failure(dummyError)
+                    
+                    sut.requestSearch(searchTerm: dummySearchTerm) { result in
+                        completionCallArgs.append(result)
+                    }
+                }
+                
+                it("should call network service once with a failure result") {
+                    let assertObject = networkServiceMock.requestCallArgs.last
+                    
+                    expect(networkServiceMock.requestCallArgs.count).to(equal(1))
+                    expect(assertObject).to(beAKindOf(ShoppingSearchDataRequest.self))
+                }
+                
+                it("should call completion with a failure result once") {
+                    expect(completionCallArgs.count).to(equal(1))
+                }
+            }
+            
+            context("when network service response returns a successfull result") {
+                
+            }
         }
     }
 }
