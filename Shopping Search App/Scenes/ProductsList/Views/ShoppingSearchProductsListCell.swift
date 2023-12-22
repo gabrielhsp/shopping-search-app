0f5585e338d9914abf5743eab57573cfd18f814c
Gabriel Henrique <gabhenrique0@gmail.com>
2023-12-21 21:09:40 -0300

    Feature: add UI Components on products list cell and set the layout

--- Shopping Search App/Scenes/ProductsList/Views/ShoppingSearchProductsListCell.swift
+++ Shopping Search App/Scenes/ProductsList/Views/ShoppingSearchProductsListCell.swift
@@ -25,15 +25,80 @@ final class ShoppingSearchProductsListCell: UITableViewCell {
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
-        stackView.alignment = .center
         stackView.spacing = 8
         
         return stackView
     }()
     
+    private let productShippingTitleStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        
+        return stackView
+    }()
+    
+    private let productFreeShippingLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 12)
+        label.text = "Frete Grátis"
+        label.textColor = .appColor(.promotion)
+        
+        return label
+    }()
+    
+    private let productTitleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.numberOfLines = 3
+        label.textColor = .appColor(.subtitle)
+        
+        return label
+    }()
+    
+    private let productPriceStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .vertical
+        stackView.spacing = .zero
+        
+        return stackView
+    }()
+    
+    private let productPriceLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.textColor = .appColor(.title)
+        
+        return label
+    }()
+    
+    private let productPriceInstallmentsLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .appColor(.title)
+        
+        return label
+    }()
+    
+    private let productConditionLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = .appColor(.action)
+        
+        return label
+    }()
+    
     // MARK: - Initializer Methods
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
     }
     
     required init?(coder: NSCoder) {
@@ -48,25 +113,47 @@ final class ShoppingSearchProductsListCell: UITableViewCell {
     }
     
     private func buildViewHierarchy() {
-        addSubview(productImageView)
-        addSubview(contentStackView)
+        contentView.addSubview(productImageView)
+        contentView.addSubview(contentStackView)
+        
+        contentStackView.addArrangedSubview(productShippingTitleStackView)
+        contentStackView.addArrangedSubview(productPriceStackView)
+        contentStackView.addArrangedSubview(productConditionLabel)
+        
+        productShippingTitleStackView.addArrangedSubview(productFreeShippingLabel)
+        productShippingTitleStackView.addArrangedSubview(productTitleLabel)
+        
+        productPriceStackView.addArrangedSubview(productPriceLabel)
+        productPriceStackView.addArrangedSubview(productPriceInstallmentsLabel)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             productImageView.heightAnchor.constraint(equalToConstant: 120),
             productImageView.widthAnchor.constraint(equalToConstant: 120),
+            productImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
             productImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            productImageView.centerYAnchor.constraint(equalTo: contentStackView.centerYAnchor),
             
+            contentStackView.heightAnchor.constraint(greaterThanOrEqualToConstant: 120),
             contentStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
             contentStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
             contentStackView.leadingAnchor.constraint(equalTo: productImageView.trailingAnchor, constant: 12),
-            contentStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 16)
+            contentStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
         ])
     }
     
     private func setupAdditionalConfiguration() {
-        
+        backgroundColor = .appColor(.background)
+        selectionStyle = .none
+    }
+}
+
+extension ShoppingSearchProductsListCell {
+    func setItem(item: ShoppingSearchProductListItem) {
+        productFreeShippingLabel.isHidden = !item.isFreeDelivered
+        productTitleLabel.text = item.name
+        productPriceLabel.text = item.promotionalPrice ?? item.originalPrice
+        productPriceInstallmentsLabel.text = item.installments
+        productConditionLabel.text = item.condition.description
     }
 }
