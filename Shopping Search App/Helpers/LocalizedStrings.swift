9c3c6b8263688579e21787c4e206220650ea6e4e
Gabriel Henrique <gabhenrique0@gmail.com>
2024-07-07 19:49:31 -0300

    Feature: migrate strings to use Strings Catalog

--- Shopping Search App/Helpers/LocalizedStrings.swift
+++ Shopping Search App/Helpers/LocalizedStrings.swift
@@ -8,16 +8,15 @@
 import Foundation
 
 struct LocalizedStrings: Equatable {
-    static let search = "Busca"
-    static let searchErrorGeneral = "Houve um erro ao realizar sua busca. Por favor, tente novamente."
-    static let searchErrorValidTerm = "Digite um termo de busca válido"
-    static let searchErrorEmptyResult = "Sua busca não retornou nenhum resultado. Por favor, tente novamente."
-    static let searchTextFieldPlaceholder = "O que você está procurando?"
-    static let searchButtonTitle = "Buscar"
-    
-    static let searchResultsTitle = "Resultados da busca"
-    static let freeTaxes = "sem juros"
-    static let freeShipping = "Frete Grátis"
-    static let productConditionNew = "Produto novo"
-    static let productConditionUsed = "Produto usado"
+    static let search = NSLocalizedString("search", comment: "Busca")
+    static let searchErrorGeneral = NSLocalizedString("search_error_general", comment: "Houve um erro ao realizar sua busca. Por favor, tente novamente.")
+    static let searchErrorValidTerm = NSLocalizedString("search_error_valid_term", comment: "Digite um termo de busca válido")
+    static let searchErrorEmptyResult = NSLocalizedString("search_error_empty_result", comment: "Sua busca não retornou nenhum resultado. Por favor, tente novamente.")
+    static let searchTextFieldPlaceholder = NSLocalizedString("search_text_field_placeholder", comment: "O que você está procurando?")
+    static let searchButtonTitle = NSLocalizedString("search_button_title", comment: "Buscar")
+    static let searchResultsTitle = NSLocalizedString("search_results_title", comment: "Resultados da busca")
+    static let freeTaxes = NSLocalizedString("free_taxes", comment: "sem juros")
+    static let freeShipping = NSLocalizedString("free_shipping", comment: "Frete Grátis")
+    static let productConditionNew = NSLocalizedString("product_condition_new", comment: "Produto novo")
+    static let productConditionUsed = NSLocalizedString("product_condition_used", comment: "Produto usado")
 }
